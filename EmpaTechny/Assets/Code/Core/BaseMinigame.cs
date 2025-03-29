2727ae0f7e50c6ad71ec5c6f9c6468e3da6427c6
Marcos Nahuel Buceta <nahuelbuceta3@gmail.com>
2025-03-29 15:23:42 -0300

    BaseMinigame Properties

--- EmpaTechny/Assets/Code/Core/BaseMinigame.cs
+++ EmpaTechny/Assets/Code/Core/BaseMinigame.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public interface IMinigame
 {
@@ -11,22 +10,62 @@ public interface IMinigame
 
 public abstract class BaseMinigame : MonoBehaviour, IMinigame
 {
+    [Header("General Settings")]
+    [SerializeField] private string minigameName;
+    [SerializeField] private Sprite backgroundImage;
+    [SerializeField] private AudioClip backgroundMusic;
+
+    [Header("UI Elements")]
+    [SerializeField] private Canvas minigameCanvas;
+    [SerializeField] private Image backgroundUI;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Button startButton;
+
     protected bool isGameActive;
+    protected int score;
 
     public virtual void StartGame()
     {
         isGameActive = true;
-        Debug.Log($"{GetType().Name} started!");
+        if (backgroundUI != null && backgroundImage != null)
+        {
+            backgroundUI.sprite = backgroundImage;
+        }
+        if (backgroundMusic != null)
+        {
+            AudioSource.PlayClipAtPoint(backgroundMusic, Vector3.zero);
+        }
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: 0";
+        }
+
+        Debug.Log($"{minigameName} started!");
     }
 
     public virtual void EndGame()
     {
         isGameActive = false;
-        Debug.Log($"{GetType().Name} ended!");
+        Debug.Log($"{minigameName} ended!");
     }
 
     public virtual void ResetGame()
     {
-        Debug.Log($"{GetType().Name} reset!");
+        score = 0;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: 0";
+        }
+        Debug.Log($"{minigameName} reset!");
+    }
+
+    public virtual void AddScore(int points)
+    {
+        score += points;
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score;
+        }
     }
 }
+
